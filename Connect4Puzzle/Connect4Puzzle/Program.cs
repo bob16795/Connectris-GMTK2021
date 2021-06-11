0145add5da7b1d777fee001deafb81dfe54200ac
sciencedoge <xxepicboixxgt@gmail.com>
2021-06-11 15:29:35 -0400

    Created a new solution

---
+++ Connect4Puzzle/Connect4Puzzle/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Connect4Puzzle
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
