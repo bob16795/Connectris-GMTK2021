03eb5119661096d2158dcb0272dbdf3c8a082251
bob16795 <prestosilver.ptp@gmail.com>
2021-06-11 13:03:07 -0400

    init dotnet

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace gmtk_GameJam2021_POG
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
