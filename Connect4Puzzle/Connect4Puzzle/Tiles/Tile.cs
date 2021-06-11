fc30e2dd616a53954d5b8f0baf70c3da5c78967d
bob16795 <prestosilver.ptp@gmail.com>
2021-06-11 16:05:28 -0400

    added sprite to tile

--- Connect4Puzzle/Connect4Puzzle/Tiles/Tile.cs
+++ Connect4Puzzle/Connect4Puzzle/Tiles/Tile.cs
@@ -3,6 +3,7 @@ using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using theNamespace.Graphics;
 
 namespace theNamespace.Tiles
 {
@@ -22,8 +23,23 @@ namespace theNamespace.Tiles
 
     public class Tile
     {
-        public static Tile[,] Map; 
+        public static Tile[,] Map = new Tile[7, 28];
+        public Point Position;
         public TileType Type;
         public TileConnection Connection;
+        public Sprite Sprite;
+
+        public void UpdateSprite() {
+            Sprite = new Sprite(new Rectangle(8 * (int)(Type), 8 * (int)(Connection), 8, 8), -new Vector2(0, 0), Color.White);
+        }
+
+        public static bool CheckHeldWeird(Tile t) {
+            if (t.Connection == TileConnection.RIGHT) {
+                return (Tile.Map[t.Position.X + 1, t.Position.Y - 1].Type == TileType.NO_TILE);
+            } else if (t.Connection == TileConnection.LEFT) {
+                return (Tile.Map[t.Position.X - 1, t.Position.Y - 1].Type == TileType.NO_TILE);
+            }
+            return false;
+        }
     }
 }
